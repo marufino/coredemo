15f17a9a902dbd3e7d3a7ed5fa485360f1eb0459
marufino <marufino@ncsu.edu>
2015-07-22 15:00:25 -0400

    fixed edit roles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
   # my custom fields are :name, :heard_how
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles_attributes: [:id, :name] }, :email, :password, :password_confirmation) }
+
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit({ roles_attributes: [:id, :name] }, :email, :password, :password_confirmation, :current_password) }
     end
 
 
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,30 +1,12 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
-  before_filter :configure_permitted_parameters
-
   # GET /users/sign_up
   def new
-
     # Override Devise default behaviour and create a profile as well
     build_resource({})
     resource.roles.build
     respond_with self.resource
   end
 
-  private
-
-  def user_params
-    params[:user].permit!
-  end
-
-=begin
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u|
-      u.permit(:email, :password, :password_confirmation, roles_attributes: [:id,:name])
-    }
-  end
-=end
 
 end
