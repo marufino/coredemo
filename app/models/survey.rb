b4608103e9f5ec0c434c944d5f0f0330ca5e1bbf
marufino <marufino@ncsu.edu>
2015-09-10 14:19:57 -0400

    added jquery form checking for survey creator

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -7,6 +7,8 @@ class Survey < ActiveRecord::Base
 
   accepts_nested_attributes_for :survey_blocks
 
+  validates :name, presence: true
+  validates :description, presence: true
 
   def self.options_for_select
     order('id').map { |e| [e.name, e.id] }
